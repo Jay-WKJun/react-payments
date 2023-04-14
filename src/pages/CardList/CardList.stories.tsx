48c75c21d1e60b1d63958839f30ba98537030e20
Woo KyungJun <natcho9010@gmail.com>
2023-04-14 16:53:41 +0900

    feat: add CardList, CardNickname story

---
+++ src/pages/CardList/CardList.stories.tsx
@@ -0,0 +1,105 @@
+import React, { JSXElementConstructor, useCallback, useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { ApplicationProvider, Card, CardList as CardListModel } from '@/contexts';
+import { postToLocalStorage } from '@/service';
+
+import { CardList } from './CardList';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'CardList/CardList',
+  component: CardList,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof CardList>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<JSXElementConstructor<CardListModel>> = (cardListProp) => {
+  const [cardList, setCardList] = useState<CardListModel>(cardListProp);
+
+  const handleCardSubmit = useCallback((card: Card, cardId: string) => {
+    setCardList((prev) => {
+      prev[cardId] = card;
+      postToLocalStorage(prev);
+
+      return { ...prev };
+    });
+  }, []);
+
+  const handleCardDelete = useCallback((card: Card, cardId: string) => {
+    setCardList((prev) => {
+      delete prev[cardId];
+      postToLocalStorage(prev);
+
+      return { ...prev };
+    });
+  }, []);
+
+  const handleCardUpdate = useCallback((newCard: Card, cardId: string) => {
+    setCardList((prev) => {
+      prev[cardId] = newCard;
+      postToLocalStorage(prev);
+
+      return { ...prev };
+    });
+  }, []);
+
+  console.log('cardList : ', cardList);
+
+  return (
+    <BrowserRouter>
+      {/* @ts-ignore */}
+      <ApplicationProvider
+        cardList={cardList}
+        onCardSubmit={handleCardSubmit}
+        onCardDelete={handleCardDelete}
+        onCardUpdate={handleCardUpdate}
+      >
+        <CardList />
+      </ApplicationProvider>
+    </BrowserRouter>
+  );
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  1: {
+    cardCompany: {
+      theme: 'red',
+      name: '애플카드',
+    },
+    cardNickname: '사과카드',
+    cardNumbers: ['1234', '3321', '5432', '8756'],
+    cardOwner: '스타브 잡스',
+    expireDates: ['12', '32'],
+    passwords: ['3', '3'],
+    securityCode: '654',
+  },
+  2: {
+    cardCompany: {
+      theme: 'mint',
+      name: '도지카드',
+    },
+    cardNickname: '화성 갈끄니까',
+    cardNumbers: ['6553', '1234', '3423', '1234'],
+    cardOwner: '일론 머스크',
+    expireDates: ['04', '65'],
+    passwords: ['2', '9'],
+    securityCode: '123',
+  },
+  3: {
+    cardCompany: {
+      theme: 'orange',
+      name: '마소카드',
+    },
+    cardNickname: '닉넴 뭐하징',
+    cardNumbers: ['4213', '5233', '1235', '4563'],
+    cardOwner: '언노운',
+    expireDates: ['02', '53'],
+    passwords: ['9', '5'],
+    securityCode: '423',
+  },
+};
---
+++ src/pages/CardNickname/CardNickname.stories.tsx
@@ -0,0 +1,59 @@
+import React, { JSXElementConstructor, useCallback, useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import { CardContext, CardProvider, ApplicationProvider, Card, CardList, CardState } from '@/contexts';
+import { postToLocalStorage } from '@/service';
+
+import { CardNicknameSetter } from './CardNicknameSetter';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'CardNicknameSetter/CardNicknameSetter',
+  component: CardNicknameSetter,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof CardNicknameSetter>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<JSXElementConstructor<CardState>> = (cardState) => {
+  const [cardList, setCardList] = useState<CardList>({});
+
+  const handleCardSubmit = useCallback((card: Card, cardId: string) => {
+    setCardList((prev) => {
+      prev[cardId] = card;
+      postToLocalStorage(prev);
+
+      return { ...prev };
+    });
+  }, []);
+
+  console.log('cardList : ', cardList);
+
+  return (
+    <BrowserRouter>
+      {/* @ts-ignore */}
+      <ApplicationProvider cardList={cardList} onCardSubmit={handleCardSubmit}>
+        <CardProvider cardInit={cardState}>
+          <CardContext.Consumer>{(store) => store && <CardNicknameSetter />}</CardContext.Consumer>
+        </CardProvider>
+      </ApplicationProvider>
+    </BrowserRouter>
+  );
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  cardCompany: {
+    value: {
+      theme: 'orange',
+      name: 'Card Company',
+    },
+  },
+  cardNumbers: [{ value: '1234' }, { value: '4567' }, { value: '8901' }, { value: '5432' }],
+  cardOwner: { value: 'Owner Name' },
+  expireDates: [{ value: '02' }, { value: '45' }],
+  passwords: [{ value: '2' }, { value: '8' }],
+  securityCode: { value: '321' },
+};
