e47cb3e014c97026fea28d45b413f765121a0c1a
Woo KyungJun <natcho9010@gmail.com>
2023-04-19 12:26:06 +0900

    fix: Appprops -> PaymentsProps

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,4 @@
 export { App as Payments } from './App';
-export type { AppProps } from './App';
+export type { AppProps as PaymentsProps } from './App';
 
 export type { Card, CardList } from './contexts/ApplicationContext';
